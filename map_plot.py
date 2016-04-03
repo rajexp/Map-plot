3e762306b9dafd244d7bfae3ee7d2a66e05b7a1d
Rajan Chauhan <rajanchauhan11@gmail.com>
2016-04-03 08:22:34 +0530

    Update map_plot.py

--- map_plot.py
+++ map_plot.py
@@ -15,49 +15,39 @@ global ini_x ,ini_y
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
-        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+        self.pack(fill=tk.BOTH,expand=True)
         self.createWidgets()
         
     def createWidgets(self):
-        global ini_x,ini_y
-        top=self.winfo_toplevel()
-        top.rowconfigure(0,weight=1)
-        top.columnconfigure(0,weight=1)
-        self.rowconfigure(0,weight=1)
-        self.columnconfigure(0,weight=1)
-        self.frame = tk.Frame(self)
-        self.frame.rowconfigure(0,weight=1)
-        self.frame.columnconfigure(0,weight=1)
-        self.frame.grid(row=0, column=0,columnspan=3,sticky=tk.N+tk.S+tk.E+tk.W)
+        global ini_x,ini_y        
+        self.entry_frame = tk.Frame(self)
+        self.entry_frame.pack(fill=tk.X,expand=False)
+        self.place=tk.StringVar()
+        self.search_label=tk.Label(self.entry_frame,text=" ")
+        self.search_label.pack(fill=tk.X,side=tk.LEFT)
+        self.w=tk.Entry(self.entry_frame,textvariable=self.place,highlightcolor="skyblue",highlightthickness=1)
+        self.w.pack(fill=tk.X,side=tk.RIGHT)
+        self.message_label=tk.Label(self.entry_frame,text="Find Places on Map : ")
+        self.message_label.pack(fill=tk.X,side=tk.RIGHT)
+        self.w.bind('<Return>',self.plott)
         
-        self.frame.bind("<Configure>", self.update)
+        self.frame = tk.Frame(self)
+        self.frame.pack(fill=tk.BOTH,expand=True)
         self.image=cv2.imread('D:\World.jpg')
         self.original=Image.fromarray(self.image)
         resized = self.original.resize((ini_x,ini_y),Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(resized)
-        
-        self.map=tk.Label(self,image=self.img)
-        self.map.rowconfigure(0,weight=1)
-        self.map.columnconfigure(0,weight=1)
-        self.map.grid(row=0,column=0,in_=self.frame,sticky=tk.N+tk.S+tk.E+tk.W)
-        
-        self.place=tk.StringVar()
-        
-        self.search_label=tk.Label(self,text="")
-        self.search_label.grid(row=1,column=0,sticky=tk.N+tk.W)
-        self.label=tk.Label(self,text="Find Places on Map : ")
-        self.label.grid(row=1,column=1,sticky=tk.N+tk.E)
-        self.w=tk.Entry(self,textvariable=self.place,highlightcolor="skyblue",highlightthickness=2)
-        self.w.grid(row=1,column=2,sticky=tk.N+tk.E)
-        self.w.bind('<Return>',self.plott)
+        self.map=tk.Label(self.frame,image=self.img)
+        self.map.pack(fill=tk.BOTH,expand=True)
+        self.bind('<Configure>',self.update)
     def coordinate(self,latitude,longitude):
         mapWidth  = self.image.shape[1]
         mapHeight = self.image.shape[0]
-        x = int((longitude+180)*(mapWidth/360))
+        x      = int((longitude+180)*(mapWidth/360))
         latRad = latitude*math.pi/180;
-        mercN = math.log(math.tan((math.pi/4)+(latRad/2)),math.e)
-        y     = int((mapHeight/2)-(mapWidth*mercN/(2*math.pi)))+238
-        cv2.circle(self.image,(x,y),3,(random.randint(50,255),random.randint(50,255),random.randint(100,255)),3)
+        mercN  = math.log(math.tan((math.pi/4)+(latRad/2)),math.e)
+        y      = int((mapHeight/2)-(mapWidth*mercN/(2*math.pi)))+238
+        cv2.circle(self.image,(x,y),3,(random.randint(50,255),random.randint(50,255),random.randint(50,255)),3)
     def get_latnlng(self,address):
         if len(address) < 1 :
             return
@@ -67,7 +57,6 @@ class Application(tk.Frame):
             uh = urllib.urlopen(url)
         except Exception,e:
             self.search_label.configure(text="Internet Connection Error")
-            print e
         data = uh.read().decode('utf-8')
         print ('Retrieved',len(data),'characters')
         tree = json.loads(data)
@@ -77,21 +66,23 @@ class Application(tk.Frame):
             lat=results[0]['geometry']['location']['lat']
             lon=results[0]['geometry']['location']['lng']
         except:
-            self.search_label.configure(text="No such place Found")
-            #No place existed hence returning these values
+            #Since Such place doesn't exist
+            self.search_label.configure(text="No Such Place Found")
             return 361,361
         return lat,lon
     def update(self,event=None):
+        print "Updated"
         self.print_on_map()
     def print_on_map(self,event=None):
-        x=self.frame.winfo_width()
-        y=self.frame.winfo_height()
+        x=self.winfo_width()
+        y=self.winfo_height()
+        print x,y
         self.original=Image.fromarray(self.image)
-        resized = self.original.resize((x-4, y-4),Image.ANTIALIAS)
+        resized = self.original.resize((x-4, y-25),Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(resized)
-        self.map=tk.Label(self,image=self.img)
-        self.map.grid(row=0,column=0,in_=self.frame,sticky=tk.N+tk.S+tk.E+tk.W)
-
+        self.map.configure(image=self.img)
+    def press(self, event=None):
+        self.w.focus_set()
     def plott(self ,event=None):
         address = str(self.w.get())
         if len(address)<1:
@@ -103,6 +94,8 @@ class Application(tk.Frame):
         x,y=self.get_latnlng(address)
         self.coordinate(x,y)
         self.print_on_map()
+        
+Toggle=True
 ini_x=800
 ini_y=600
 app = Application()
